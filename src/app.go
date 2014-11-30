1ba49b281a13567ca64e7491fa9f5380ba9ed38e
Tristan O'Neil <tristanoneil@gmail.com>
2014-11-30 16:51:24 -0500

    Setup dotenv.

--- src/app.go
+++ src/app.go
@@ -1,24 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 	"github.com/gorilla/sessions"
 	"github.com/jmoiron/sqlx"
+	_ "github.com/joho/godotenv/autoload"
 	"github.com/justinas/nosurf"
 	_ "github.com/lib/pq"
 )
 
 var (
-	db *sqlx.DB
+	db    *sqlx.DB
+	store *sessions.CookieStore
 )
 
-var store = sessions.NewCookieStore([]byte("something-very-secret"))
-
 func main() {
-	db, _ = sqlx.Connect("postgres", "dbname=badger sslmode=disable")
+	db, _ = sqlx.Connect(
+		"postgres",
+		fmt.Sprintf("dbname=%s sslmode=disable", os.Getenv("DATABASE")),
+	)
+
+	store = sessions.NewCookieStore([]byte(os.Getenv("SESSION_SECRET")))
 
 	router := mux.NewRouter()
 	router.HandleFunc("/signup", signup).Methods("GET", "POST")
