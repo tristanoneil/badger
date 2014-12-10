c0c9b748f8f8b8bfa2cac9d56f6a248a2e88f932
Tristan O'Neil <tristanoneil@gmail.com>
2014-12-10 12:05:56 -0500

    Add ace editor.

--- build.sh
+++ build.sh
@@ -1,5 +1,2 @@
-( cd models && rice embed-go )
-( cd routes && rice embed-go )
 GOOS=linux GOARCH=386 go build -o builds/badger.linux
-rm -rf routes/*rice-box.go
-rm -rf models/*rice-box.go
+rice -i ./models -i ./routes append --exec builds/badger.linux
