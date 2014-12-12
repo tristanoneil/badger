b23ab130b599137eca2d88255b1e2033cf79f9c8
Tristan O'Neil <tristanoneil@gmail.com>
2014-12-12 13:33:24 -0500

    Build OSX version.

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 GOOS=linux GOARCH=386 go build -o builds/badger.linux
+GOOS=darwin GOARCH=386 go build -o builds/badger.mac
 rice -i ./models -i ./routes append --exec builds/badger.linux
+rice -i ./models -i ./routes append --exec builds/badger.mac
